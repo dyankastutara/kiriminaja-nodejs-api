02cd09f45e91da735d83352cf058b8fc67878450
Dyan Kastutara <dyankastutara19@gmail.com>
2024-02-02 20:36:30 +0700

    add controller

--- controllers/kiriminaja.js
+++ controllers/kiriminaja.js
@@ -10,13 +10,7 @@ module.exports = {
    checkcost: {
       express: async (req, res) => {
          try{
-            const response = await axios.post(`${process.env.HOST_KIRIMINAJA}/api/mitra/shipping_price`,{
-              origin: req.body.origin,
-              destination: req.body.destination,
-              weight: req.body.weight,
-              insurance: req.body.insurance,
-              item_value: req.body.item_value
-            },{
+            const response = await axios.post(`${process.env.HOST_KIRIMINAJA}/api/mitra/shipping_price`,req.body,{
               headers: {
                 Authorization : process.env.API_KEY_KIRIMINAJA
               }
@@ -29,25 +23,7 @@ module.exports = {
       },
       instant: async (req, res) => {
          try{
-            const response = await axios.post(`${process.env.HOST_KIRIMINAJA}/api/mitra/v3/instant/pickup/request`,{
-               service: ["gosend","grab_express"],
-               item_price: req.body.item_price,
-               origin: {
-                  lat: req.body.origin.lat,
-                  long: req.body.origin.long,
-                  address: req.body.origin.address
-               },
-               destination: {
-                  lat: req.body.destination.lat,
-                  long: req.body.destination.long,
-                  address: req.body.destination.address
-               },
-               weight: req.body.weight,
-               packages: [],
-               vehicle: {
-                  "name":"motor"
-               }
-            },{
+            const response = await axios.post(`${process.env.HOST_KIRIMINAJA}/api/mitra/v3/instant/pickup/request`,req.body,{
                headers: {
                   Authorization : process.env.API_KEY_KIRIMINAJA
                }
@@ -74,17 +50,7 @@ module.exports = {
    pickup: {
       express: async (req, res) => {
         try {
-          const response = await axios.post(`${process.env.HOST_KIRIMINAJA}/api/mitra/v2/request_pickup`, {
-            name : req.body.name,
-            address : req.body.address,
-            phone : req.body.phone,
-            kecamatan_id : req.body.kecamatan_id,
-            packages : req.body.packages,
-            zipcode : req.body.zipcode,
-            schedule : req.body.schedule,
-            drop: req.body.drop,
-            packages: req.body.packages
-          },{
+          const response = await axios.post(`${process.env.HOST_KIRIMINAJA}/api/mitra/v2/request_pickup`, req.body,{
             headers: {
               Authorization : process.env.API_KEY_KIRIMINAJA
             }
@@ -96,9 +62,7 @@ module.exports = {
       },
       instant: async (obj) => {
         try {
-          const response = await axios.post(`${process.env.HOST_KIRIMINAJA}/api/mitra/v2/request_pickup`, {
-            packages: req.body.packages
-          },{
+          const response = await axios.post(`${process.env.HOST_KIRIMINAJA}/api/mitra/v2/request_pickup`, req.body,{
             headers: {
               Authorization : process.env.API_KEY_KIRIMINAJA
             }
