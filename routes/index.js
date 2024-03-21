40a62bb703c70f632e2b311932577d18e3991c6b
Dyan Kastutara <dyankastutara19@gmail.com>
2024-03-21 11:09:48 +0700

    add set_callback

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const controller = require('../controllers');
+const controller = require("../controllers");
 
-router.post('/coveragearea/provinces', controller.coveragearea.province);
-router.post('/coveragearea/cities', controller.coveragearea.city);
-router.post('/coveragearea/district', controller.coveragearea.district);
-router.post('/checkcost/express', controller.checkcost.express);
-router.post('/checkcost/instant', controller.checkcost.instant);
-router.post('/schedules', controller.schedules);
-router.post('/pickup/express', controller.pickup.express);
-router.post('/pickup/instant', controller.pickup.instant);
-router.post('/tracking/express', controller.tracking.express);
-router.post('/void/express', controller.void.express);
-router.post('/void/instant', controller.void.instant);
+router.post("/coveragearea/provinces", controller.coveragearea.province);
+router.post("/coveragearea/cities", controller.coveragearea.city);
+router.post("/coveragearea/district", controller.coveragearea.district);
+router.post("/checkcost/express", controller.checkcost.express);
+router.post("/checkcost/instant", controller.checkcost.instant);
+router.post("/schedules", controller.schedules);
+router.post("/pickup/express", controller.pickup.express);
+router.post("/pickup/instant", controller.pickup.instant);
+router.post("/tracking/express", controller.tracking.express);
+router.post("/void/express", controller.void.express);
+router.post("/void/instant", controller.void.instant);
+router.post("/set_callback", controller.set_callback);
 
 module.exports = router;
