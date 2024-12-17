56b4a45e308a76a10f4cce21e5963d36443ca508
Dyan Kastutara <dyankastutara19@gmail.com>
2024-12-17 09:20:54 +0700

    change subdistrict to district, and add subdistrict / kelurahan

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,12 @@
-require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 
 const controller = require("../controllers");
-router.get("/", (req, res) => {
-  res.redirect(process.env.HOST_REDIRECT);
-});
-router.get("/status", (req, res) => {
-  var IP = req.ip;
-  var LOCAL_IP = req.socket.localAddress;
 
-  res.json({
-    status: process.env.DEBUG === "true" ? "development" : "production",
-    ka_host: process.env.HOST_KIRIMINAJA,
-    port: process.env.PORT,
-    ip: IP,
-    local_address: LOCAL_IP,
-    connected: true,
-  });
-});
-//coverage area
 router.post("/coveragearea/provinces", controller.coveragearea.province);
 router.post("/coveragearea/cities", controller.coveragearea.city);
 router.post("/coveragearea/district", controller.coveragearea.district);
 router.post("/coveragearea/subdistrict", controller.coveragearea.subdistrict);
-router.post("/coveragearea/search", controller.coveragearea.search);
-//express
-router.post("/express/schedules", controller.express.schedule);
-router.post("/express/pricing", controller.express.pricing);
-router.post("/express/pickup/request", controller.express.pickup);
-router.post("/express/tracking", controller.express.tracking);
-router.post("/express/void", controller.express.void);
-//instant
-router.post("/instant/pricing", controller.instant.pricing);
-router.post("/instant/pickup/request", controller.instant.pickup);
-router.post("/instant/tracking", controller.instant.tracking);
-router.post("/instant/find_driver", controller.instant.find_driver);
-router.post("/instant/void", controller.instant.void);
-//set-callback
-router.post("/set_callback", controller.set_callback);
-//last
 router.post("/checkcost/express", controller.checkcost.express);
 router.post("/checkcost/instant", controller.checkcost.instant);
 router.post("/schedules", controller.schedules);
