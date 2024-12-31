139dab4965e76eb8efbe19182815f5e600d7bde9
Dyan Kastutara <dyankastutara19@gmail.com>
2024-12-31 09:27:04 +0700

    modified

--- routes/index.js
+++ routes/index.js
@@ -27,13 +27,13 @@ router.post("/instant/void", controller.instant.void);
 //set-callback
 router.post("/set_callback", controller.set_callback);
 //last
-router.post("/checkcost/express", controller.checkcost.express);
-router.post("/checkcost/instant", controller.checkcost.instant);
-router.post("/schedules", controller.schedules);
-router.post("/pickup/express", controller.pickup.express);
-router.post("/pickup/instant", controller.pickup.instant);
-router.post("/tracking/express", controller.tracking.express);
-router.post("/void/express", controller.void.express);
-router.post("/void/instant", controller.void.instant);
+// router.post("/checkcost/express", controller.checkcost.express);
+// router.post("/checkcost/instant", controller.checkcost.instant);
+// router.post("/schedules", controller.schedules);
+// router.post("/pickup/express", controller.pickup.express);
+// router.post("/pickup/instant", controller.pickup.instant);
+// router.post("/tracking/express", controller.tracking.express);
+// router.post("/void/express", controller.void.express);
+// router.post("/void/instant", controller.void.instant);
 
 module.exports = router;
